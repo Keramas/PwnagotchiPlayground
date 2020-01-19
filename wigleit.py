7ba7ac49f6646f2a3d077c27f82acb8a81392008
Devin Casadey <devincasadey@gmail.com>
2020-01-19 15:23:19 -0800

    Update wigleit.py

--- wigleit.py
+++ wigleit.py
@@ -4,20 +4,28 @@ import requests
 import argparse
 import json
 import sys
+import os
 
 wigleURL="https://api.wigle.net/api/v2/network/search?"
 token = "ADD YOUR WIGLE API BASIC AUTH KEY HERE"
 
+essid_list = []
+bssid_list = []
+
 def get_args():
     parser = argparse.ArgumentParser(description="Find an AP location with Wigle API", epilog="Wigle it, just a little bit!\n")
+    parser.add_argument('-m', '--mode', type=str, help="Select either 0 or 1. 0 is directory mode, whereas 1 is single query mode.", required=True)
     parser.add_argument('-hm', '--latmin', type=str, help="", required=True)
     parser.add_argument('-hM', '--latmax', type=str, help="", required=True)
     parser.add_argument('-vm', '--longmin', type=str, help="", required=True)
-    parser.add_argument('-vM', '--longmax', type=str, help="", required=True)
-    parser.add_argument('-e', '--essid', type=str, help="", required=True)
-    parser.add_argument('-b', '--bssid', type=str, help="", required=True)
+    parser.add_argument('-vM', '--longmax', type=str, help="", required=True) 
+    parser.add_argument('-d', '--directory', type=str, help="", required=False)
+    parser.add_argument('-e', '--essid', type=str, help="", required=False)
+    parser.add_argument('-b', '--bssid', type=str, help="", required=False)
 
     args = parser.parse_args()
+    mode = args.mode
+    directory = args.mode
     latmin = args.latmin
     latmax = args.latmax
     longmin = args.longmin
@@ -25,7 +33,16 @@ def get_args():
     essid = args.essid
     bssid = args.bssid
 
-    return latmin,latmax,longmin,longmax,essid,bssid
+    return mode,directory,latmin,latmax,longmin,longmax,essid,bssid
+
+
+def parseFiles(directory):
+    for file in os.listdir(directory):
+        essid = file.split(".")[0].split("_")[0]    
+        bssid = file.split(".")[0].split("_")[1]
+        bssid_mac = ':'.join([bssid[i:i+2] for i in range(0, len(bssid), 2)])
+        essid_list.append(essid)
+        bssid_list.append(bssid_mac)
 
 
 def search(latmin,latmax,longmin,longmax,essid,bssid):
@@ -45,6 +62,8 @@ def parseResults(results):
         sys.exit(0)
 
     else:
+        print("[+] AP found. Parsing data...")
+
         latitude = results["results"][0]["trilat"]
         longitude = results["results"][0]["trilong"]
         ssid = results["results"][0]["ssid"]
@@ -60,12 +79,25 @@ def parseResults(results):
 
 
 def main():
-    latmin,latmax,longmin,longmax,essid,bssid = get_args()
-    print("[+] Querying Wigle for data...")
-    results = search(latmin,latmax,longmin,longmax,essid,bssid)
-    print("[+] AP found. Parsing results...")    
-    parseResults(results)
+    latmin,latmax,longmin,longmax,essid,bssid,directory,mode = get_args()
+
+    if mode == 0:
+        parseFiles(directory)
+        for i in essid_list:
+            for j in bssid_list:
+                results = search(latmin,latmax,longmin,longmax,i,j)
+                parseResults(results)
+
+    elif mode == 1:
+        results = search(latmin,latmax,longmin,longmax,essid,bssid)
+        parseResults(results)
+
+    else:
+        print("[!] Mode not selected. Please select either mode 0 or mode 1.")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
     main()
+    print("[!] Done.")
+    sys.exit(0)
